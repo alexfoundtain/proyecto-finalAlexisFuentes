1ceafc513c350111b3c283cfc5ce10614eff5021
alexis.fuentes-at-514910645081 <alexis.foundtain@gmail.com>
2022-04-25 21:38:00 -0600

    actualizando

---
+++ proyecto-final-alexis-fuentes/src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import './ItemCount.css';
+
+export const ItemCount = ({stock, initial, onAdd}) => {
+    const [count, setCount] =useState(initial);
+    function handlePlusButton(){
+        if (count < stock) {
+            setCount( count + 1 );
+        }
+    }
+    function handleMinusButton(){
+        if (count>0) {
+            setCount( count - 1);
+        }
+    }
+  return (
+    <div className='item-count-container-grandfa'>
+        <div className='item-count-container-fa'>
+                <button onClick={() => handleMinusButton()}>-</button>
+                <input readOnly type="text" value={count} />
+                <button onClick={() => handlePlusButton()}>+</button>
+         </div>
+         <div className='item-count-container-fa1'>   
+            <button onClick={() =>(count <=stock) && onAdd()}>Agregar al carrito </button>
+        </div>
+
+
+    </div>
+  )
+}
+export default ItemCount;
